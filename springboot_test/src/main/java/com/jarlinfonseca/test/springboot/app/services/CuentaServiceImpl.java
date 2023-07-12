5793aa1ce0589f049f8535083e0cd05b237d163d
Jarlin Fonseca <jarlin.fonsecab@pragma.com.co>
2023-07-12 17:19:22 -0500

    Uso de anotaciones de spring @MockBean y @Autowired

--- springboot_test/src/main/java/com/jarlinfonseca/test/springboot/app/services/CuentaServiceImpl.java
+++ springboot_test/src/main/java/com/jarlinfonseca/test/springboot/app/services/CuentaServiceImpl.java
@@ -4,9 +4,10 @@ import com.jarlinfonseca.test.springboot.app.models.Banco;
 import com.jarlinfonseca.test.springboot.app.models.Cuenta;
 import com.jarlinfonseca.test.springboot.app.repositories.BancoRepository;
 import com.jarlinfonseca.test.springboot.app.repositories.CuentaRepository;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-
+@Service
 public class CuentaServiceImpl implements CuentaService {
     private CuentaRepository cuentaRepository;
     private BancoRepository bancoRepository;
